a3ed54e083f1a6f009423ce6a87ae89467413787
Tomasz Krawczyk <ToKrawczyk@grupa.lukas>
2020-11-14 11:57:55 +0100

    filtering finished

--- Product/views.py
+++ Product/views.py
@@ -13,16 +13,33 @@ def product_category(request, category_path):
     filters_dict = {}
     filter_submited_dict = {}
     product_list = Product.objects.filter(product_category=product_category)
+    any_mapping_word = 'dowolny'
 
     for k, v in product_category.attributes_json.items():
         filters_dict[k] = {}
         filters_dict[k]['value'] = v
         filters_dict[k]['type'] = str(type(v))
         filters_dict[k]['choices'] = []
-        if type(v) in (str, bool):
+        filters_dict[k]['default'] = {}
+        if type(v) in (int,):
+            filters_dict[k]['default'] = {}
+            filters_dict[k]['default']['min'] = request.GET.get(k + '_min', '')
+            filters_dict[k]['default']['max'] = request.GET.get(k + '_max', '')
+        if type(v) in (str,):
+            filters_dict[k]['default'] = []
             for product in product_list:
                 filters_dict[k]['choices'].append(product.attributes_json[k])
-            filters_dict[k]['choices'].append('dowolny')
+            filters_dict[k]['choices'].append(any_mapping_word)
+            filters_dict[k]['default'] = []
+            for choice in filters_dict[k]['choices']:
+                if request.GET.get(k + '_' + choice) or request.GET.get(k + '_' + any_mapping_word):
+                    filters_dict[k]['default'].append(choice)
+        if type(v) in (bool,):
+            for product in product_list:
+                filters_dict[k]['choices'].append(product.attributes_json[k])
+            filters_dict[k]['choices'].append(any_mapping_word)
+            filters_dict[k]['default'] = request.GET.get(k + '_bool')
+
 
     if request.GET.get('filter_active'):
         filter_submited_dict = {}
@@ -34,11 +51,14 @@ def product_category(request, category_path):
                     filter_submited_dict[k]['_min'] = request.GET.get(k + '_min')
                 if request.GET.get(k + '_max'):
                     filter_submited_dict[k]['_max'] = request.GET.get(k + '_max')
-            if 'str' in v['type'] or 'bool' in v['type']:
+            if 'str' in v['type']:
                 filter_submited_dict[k] = []
                 for choice in v['choices']:
                     if request.GET.get(k + '_' + choice):
                         filter_submited_dict[k].append(choice)
+            if 'bool' in v['type']:
+                filter_submited_dict[k] = request.GET.get(k + '_bool')
+
 
         product_list_filtered = []
         for product in product_list:
@@ -46,13 +66,16 @@ def product_category(request, category_path):
             for k, v in filter_submited_dict.items():
                 if 'int' in filters_dict[k]['type']:
                     if '_min' in v.keys():
-                        if product.attributes_json[k] < int(v['_min']):
+                        if product.attributes_json[k] <= int(v['_min']):
                             pass_filtering = False
                     if '_max' in v.keys():
-                        if product.attributes_json[k] > int(v['_max']):
+                        if product.attributes_json[k] >= int(v['_max']):
                             pass_filtering = False
                 if 'str' in filters_dict[k]['type'] or 'bool' in filters_dict[k]['type']:
-                    if str(product.attributes_json[k]) not in v and 'dowolny' not in v:
+                    if str(product.attributes_json[k]) not in v and any_mapping_word not in v:
+                        pass_filtering = False
+                if 'bool' in filters_dict[k]['type']:
+                    if str(product.attributes_json[k]) != v and v != any_mapping_word:
                         pass_filtering = False
 
             if pass_filtering:
