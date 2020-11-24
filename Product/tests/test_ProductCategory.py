603d31fd73829ffc4d8b193085aaff205ddc148b
ToKrawczyk <tokrawczyk@credit-agricole.pl>
2020-11-24 20:28:49 +0100

    1. UNIT_TESTS ProductCategory: update_category_fields (patchowana metoda make_root() - do dokonczenia)

--- Product/tests/test_ProductCategory.py
+++ Product/tests/test_ProductCategory.py
@@ -1,5 +1,6 @@
 import pytest
 from pytest_mock import mocker
+from unittest.mock import patch
 from django.forms import ValidationError
 
 from Product.models import ProductCategory, pre_save_product_category
@@ -10,6 +11,28 @@ def product_category_instance():
     product_category = ProductCategory()
     yield product_category
     del product_category
+
+@pytest.fixture(scope = 'function')
+def product_category_instances():
+    product_category1 = ProductCategory()
+    product_category2 = ProductCategory()
+    yield (product_category1, product_category2)
+    del product_category1
+    del product_category2
+
+# test pre_save_product_category signal called methods order
+def test_pre_save_product_category_signal_called_methods_order(mocker):
+    product_category_mock = mocker.Mock()
+    calls = [mocker.call.validate_root_category(), mocker.call.validate_attributes_types(),
+             mocker.call.update_category_fields(), mocker.call.inherit_attributes()]
+    pre_save_product_category(mocker.Mock, product_category_mock)
+    product_category_mock.assert_has_calls(calls)
+
+# test validate_root_category
+def test_if_validate_root_category_raises_exception_correctly(product_category_instance):
+    with pytest.raises(ValidationError):
+        product_category_instance.root_category = product_category_instance
+        product_category_instance.validate_root_category()
 
 
 # test validate_attributes_types
@@ -48,33 +71,37 @@ def test_if_validate_attributes_types_raises_exception_correctly_when_none_attri
         product_category_instance.attributes_json = {'atr1': None}
         product_category_instance.validate_attributes_types()
 
-# test validate_root_category
-def test_if_validate_root_category_raises_exception_correctly(product_category_instance):
-    with pytest.raises(ValidationError):
-        product_category_instance.root_category = product_category_instance
-        product_category_instance.validate_root_category()
+# test update_category_fields
+def test_update_category_fields_method_when_none_root_category(product_category_instances):
+    product_category_instances[0].category_name = 'Test category'
+    product_category_instances[0].update_category_fields()
+    assert product_category_instances[0].category_path == product_category_instances[0].category_name
 
+def test_update_category_fields_method_when_specific_root_category(product_category_instances):
+    product_category_instances[0].category_name = 'Test category'
+    product_category_instances[1].category_path = 'Root category'
 
+    product_category_instances[0].root_category = product_category_instances[1]
+    with patch('Product.models.ProductCategory.make_root'): # TODO: Nie przetestowane metody: make_root i un_root
+       product_category_instances[0].update_category_fields()
+    assert product_category_instances[0].category_path == product_category_instances[1].category_path + '-' + product_category_instances[0].category_name
 
-@pytest.fixture(scope = 'module')
-def product_category_instances():
-    product_category1 = ProductCategory()
-    product_category2 = ProductCategory()
-    yield (product_category1, product_category2)
-    del product_category1
-    del product_category2
+def test_update_category_fields_method_when_none_attributes_old_json(product_category_instances):
+    product_category_instances[0].attributes_json = {"test": 2}
+    with patch('Product.models.ProductCategory.make_root'):
+       product_category_instances[0].update_category_fields()
+    assert product_category_instances[0].attributes_old_json == product_category_instances[0].attributes_json
 
-
-# test pre_save_product_category signal called methods order
-def test_pre_save_product_category_signal_called_methods_order(mocker, product_category_instances):
-    product_category_mock = mocker.Mock()
-    calls = [mocker.call.validate_root_category(), mocker.call.validate_attributes_types(),
-             mocker.call.update_category_fields(), mocker.call.inherit_attributes()]
-    pre_save_product_category(mocker.Mock, product_category_mock)
-    product_category_mock.assert_has_calls(calls)
+def test_update_category_fields_method_when_specific_attributes_old_json(product_category_instances):
+    product_category_instances[0].attributes_json = {"test": 2}
+    product_category_instances[0].attributes_old_json = {"test": 1}
+    expected_value = product_category_instances[0].attributes_old_json
+    with patch('Product.models.ProductCategory.make_root'):
+       product_category_instances[0].update_category_fields()
+    assert product_category_instances[0].attributes_old_json == expected_value
 
 # test inherit_attributes
-def test_inherit_attributes_method_works_correctly(product_category_instances): # TUTAJ TRZEBA UZYC MOCKOWANIA JAKIEGOS
+def test_inherit_attributes_method_works_correctly(product_category_instances):
     product_category_instances[0].attributes_json = {"test": 1}
     product_category_instances[1].root_category = product_category_instances[0]
     product_category_instances[1].attributes_json = {"dane": 0}
@@ -104,7 +131,7 @@ def test_inherit_attributes_method_works_correctly(product_category_instances): 
 #     del product_category2
 
 # testy validate_attributes_types
-# def test_inherit_attributes(product_category_instances): # TUTAJ TRZEBA UZYC MOCKOWANIA JAKIEGOS
+# def test_inherit_attributes(product_category_instances):
 #     product_category_instances[0].attributes_json = {"test": 1}
 #     product_category_instances[1].root_category = product_category_instances[1]
 #     product_category_instances[1].attributes_json = {"dane": 0}
